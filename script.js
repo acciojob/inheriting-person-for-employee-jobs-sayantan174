93f0717978ef4df19d874748f3a60a124d238a23
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-21 18:17:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function Person(name, age) {
 Person.prototype.greet = function () {
 	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old.")
 }
-Employee.prototype=Person;
+Person.prototype=Employee;
 function Employee(name, age, jobTitle) {}
 Employee.prototype.jobGreet = function () {
 	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
