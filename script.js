e41e41db58153e80ccb8e3b8fda2aac71274a978
Sayantan Mandal <sayantanmandal174@gmail.com>
2023-05-21 18:13:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
 // complete this js code
-function Person(name, age) {}
-
+function Person(name, age) {
+	
+}
+Person.prototype.greet = function () {
+	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old.")
+}
+Employee.prototype=Person;
 function Employee(name, age, jobTitle) {}
-
+Employee.prototype.jobGreet = function () {
+	console.log("Hello, my name is "+this.name+", I am "+this.age+" years old, and my job title is "+this.jobTitle+".")
+}
 // Do not change code below this line
 window.Person = Person;
 window.Employee = Employee;
